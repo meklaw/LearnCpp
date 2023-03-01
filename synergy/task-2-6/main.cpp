ec54e153d35084805ed8fa6bcea25051c1b41503
Никита Ульянов <nik.ule@yandex.ru>
2023-03-02 02:13:26 +0300

    refactor

--- synergy/task-2-6/main.cpp
+++ synergy/task-2-6/main.cpp
@@ -8,22 +8,18 @@ int main() {
     cin >> day >> month >> year;
 
     bool isLeapYear = false;
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                isLeapYear = true;
-            }
-        } else {
-            isLeapYear = true;
-        }
+    if (year % 4 == 0 || year % 400 == 0) {
+        isLeapYear = true;
     }
     cout << "Leap year: " << (isLeapYear ? "Yes" : "No") << endl;
 
-    string animals[] = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"};
+    string animals[] = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse",
+                        "Sheep"};
     int animalIndex = (year - 4) % 12;
     cout << "Eastern calendar year: " << animals[animalIndex] << endl;
 
-    string signs[] = {"Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"};
+    string signs[] = {"Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio",
+                      "Sagittarius", "Capricorn"};
     int signIndex = month - (day < 21 ? 2 : 1);
     if (signIndex < 0) {
         signIndex += 12;
